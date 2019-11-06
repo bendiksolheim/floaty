a21a5776837c8429b45ce8ebd5d6850faa37a848
Bendik Solheim <hello@bendik.dev>
2019-11-06 23:01:15 +0100

    Initial Commit

---
+++ Floaty/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Floaty
+//
+//  Created by Bendik Solheim on 06/11/2019.
+//  Copyright © 2019 Bendik Solheim. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
