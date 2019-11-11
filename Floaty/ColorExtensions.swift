dace7e951df0fdbffa8d7133635d9bfa4aa29af2
Bendik Solheim <hello@bendik.dev>
2019-11-11 21:25:10 +0100

    Color extension for parsing hex string

---
+++ Floaty/ColorExtensions.swift
@@ -0,0 +1,26 @@
+//
+//  ColorExtensions.swift
+//  Floaty
+//
+//  Created by Bendik Solheim on 11/11/2019.
+//  Copyright © 2019 Bendik Solheim. All rights reserved.
+//
+
+import SwiftUI
+
+extension Color {
+    init(hex: String) {
+        let scanner = Scanner(string: hex)
+        scanner.scanLocation = 0
+        var rgbValue: UInt64 = 0
+        scanner.scanHexInt64(&rgbValue)
+
+        let r = (rgbValue & 0xff0000) >> 16
+        let g = (rgbValue & 0xff00) >> 8
+        let b = rgbValue & 0xff
+
+
+        self.init(red: Double(r) / 0xff, green: Double(g) / 0xff, blue: Double(b) / 0xff)
+
+    }
+}
