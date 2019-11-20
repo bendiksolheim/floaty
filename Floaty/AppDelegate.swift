eaf8ffd077f59f3a90fab59ad595bb49d1093616
Bendik Solheim <hello@bendik.dev>
2019-11-20 21:30:01 +0100

    Add "Open location" to menu programmatically

--- Floaty/AppDelegate.swift
+++ Floaty/AppDelegate.swift
@@ -25,12 +25,32 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         window.makeKeyAndOrderFront(nil)
         
         window.level = .floating
+        
+        if let menu = NSApp.mainMenu {
+            modify(menu: menu)
+        }
+    }
+    
+    func modify(menu: NSMenu) {
+        menu.removeItem(at: 4) // View
+        menu.removeItem(at: 3) // Format
+        
+        if let file = menu.item(at: 1) {
+            let fileMenu = file.submenu!
+            fileMenu.removeItem(at: 1) // Open
+            fileMenu.removeItem(at: 0) // New
+            let openLocation = NSMenuItem(title: "Open location", action: #selector(AppDelegate.test(sender:)), keyEquivalent: "l")
+            openLocation.keyEquivalentModifierMask = .command
+            openLocation.isEnabled = true
+            fileMenu.insertItem(openLocation, at: 0)
+        }
+    }
+    
+    @objc func test(sender: NSMenuItem) {
+        print("lol")
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
-
-
 }
-
