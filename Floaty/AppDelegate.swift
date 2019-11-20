91c6e99d2e2dc1124524ebea4b5a5e7264636a73
Bendik Solheim <hello@bendik.dev>
2019-11-20 21:29:26 +0100

    Remove unsupported stylemask

--- Floaty/AppDelegate.swift
+++ Floaty/AppDelegate.swift
@@ -17,7 +17,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
         window = CustomWindow(
             contentRect: NSRect(x: 0, y: 0, width: 480, height: 300),
-            styleMask: [.closable, .resizable, .hudWindow],
+            styleMask: [.closable, .resizable],
             backing: .buffered, defer: false)
         window.center()
         window.setFrameAutosaveName("Main Window")
